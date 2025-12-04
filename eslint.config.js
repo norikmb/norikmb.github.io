52e158adb476af7b3e584fc7b8da213897eedfa5
norikmb <nori.k3636@gmail.com>
2025-12-05 01:02:31 +0900

    feat: add OSV-Scanner workflow and ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import astroParser from "astro-eslint-parser";
+import astroPlugin from "eslint-plugin-astro";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      ".astro",
+      "pnpm-lock.yaml",
+      "public",
+    ],
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+    plugins: {
+      astro: astroPlugin,
+    },
+    rules: {
+      ...astroPlugin.configs.recommended.rules,
+      "astro/no-conflict-set-directives": "error",
+      "astro/no-unused-define-vars-in-style": "warn",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  prettier,
+];
